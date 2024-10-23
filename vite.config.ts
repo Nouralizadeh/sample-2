3ba83233d928555734553309bcca5afe8d045844
Shadi Alizadeh <37235181+Nouralizadeh@users.noreply.github.com>
2024-10-23 11:10:55 +0330

    Correct build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // تغییر مسیر خروجی به build
+    outDir: 'build',
   },
-})
+});
